7c9b30beb585cf7a2572991761bd12c9fe9ee0f9
Alexander1988 <ultramagnus88@gmail.com>
2020-02-05 23:48:02 +0300

    Добавил текст рецепта, scroll, заметку. Текст рецепта соотв. картинке. Когда работает рандомайзер текст меняется вместе с картинкой

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,13 +5,18 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import java.util.*
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage: ImageView
+    lateinit var diceRecipe:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,23 +24,51 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+        addNickname(it)
+        }
 
         diceImage = findViewById(R.id.dice_image)
+        diceRecipe=findViewById(R.id.recipe_text)
     }
 
     private fun rollDice() {
 
         val randomInt = Random().nextInt(6)+1
         val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            1 -> R.drawable.cake
+            2 -> R.drawable.fish
+            3 -> R.drawable.porridge
+            4 -> R.drawable.meat
+            5 -> R.drawable.sausage
+            else -> R.drawable.shashlik
+        }
+        val stringResource= when (randomInt){
+            1 -> R.string.recipe
+            2 -> R.string.recipe2
+            3 -> R.string.recipe3
+            4 -> R.string.recipe4
+            5 -> R.string.recipe5
+            else -> R.string.recipe6
+
         }
 
         diceImage.setImageResource(drawableResource)
+        diceRecipe.setText(stringResource)
 
     }
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.note_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 }
